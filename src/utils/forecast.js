a1d9460622905e5bb2d5772389a46cbd32aba47e
Himanshu Singhal <himanshusinghal@BPSSMBA031s-MacBook-Air.local>
2020-04-07 10:06:05 +0530

    Updated Output

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,7 @@ const forecast = (latitude, longitude, callback) => {
                 callback('Unable to find location', undefined)
             }
             else {
-                callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+                callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. The High today is ' + body.daily.data[0].temperatureHigh + ' degrees with a low of ' + body.daily.data[0].temperatureLow + ' degrees. There is a ' + body.currently.precipProbability + '% chance of rain.')
             }
         })
 }
