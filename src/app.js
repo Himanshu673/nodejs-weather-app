8bf49bd7d0a7315a46d96c72bd1950a584200b9b
Himanshu Singhal <himanshusinghal@BPSSMBA031s-MacBook-Air.local>
2020-04-07 08:11:03 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,102 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+// Define Paths for Express Config
+const directoryPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialPath = path.join(__dirname, '../templates/partials')
+
+// Setup Handlebar engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+
+hbs.registerPartials(partialPath)
+
+// Setup Static Directory to serve 
+app.use(express.static(directoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App', 
+        name: 'Himanshu Singhal'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About App', 
+        name: 'Himanshu Singhal'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'Urgent Help Needed!',
+        title: 'Help Page',
+        name: 'Himanshu Singhal'
+    })
+})
+
+
+app.get('/weather', (request, response) => {
+    if(!request.query.address) {
+        return response.send({
+            error: "Must Provide a Address Value!"
+        })
+    }
+
+    geocode( request.query.address , (error, { latitude, longitude, location} = {}) => {
+        if(error) {
+            return response.send({error})
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error) {
+                return response.send({error})
+            }
+            response.send({
+                forecast: forecastData,
+                location,
+                address: request.query.address
+            })
+        })
+    })
+})
+
+app.get('/products', (request, response) => {
+    if(!request.query.search) {
+        return response.send({
+            error: "Must Provide a Search Value!"
+        })
+    }
+
+    response.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('error', {
+        message: 'Help Article not found',
+        title: 'Help Page',
+        name: 'Himanshu Singhal'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('error', {
+        message: 'Page not Found',
+        title: 'Weather',
+        name: 'Himanshu Singhal'
+    })
+})
+
+
+app.listen(3000, () => {
+    console.log("Server is up on port 3000")
+})
